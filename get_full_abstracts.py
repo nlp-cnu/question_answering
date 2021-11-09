9bd3d497001536923b86b9cbbe69be2463b0a1f0
DanielSims1 <danielssims@hotmail.com>
2021-11-09 18:32:27 -0500

    optimized

--- get_full_abstracts.py
+++ get_full_abstracts.py
@@ -1,67 +1,84 @@
 import json
 import os
+import pickle
 from lxml import etree as ET
 
-# Retrieve the abstracts for each document (same level as pagination)
-# IF simple indexing then it is much easier to get abstracts
-# IF NOT, then brute force
-def get_abstracts_from_db(document_urls,pubmed_db_dir,simple=False):
-    # Get the last part of the pubmed document url [[  http://www.ncbi.nlm.nih.gov/pubmed/23959273 -> 23959273  ]]
-    ids = [url.rsplit('/', 1)[-1] for url in document_urls]
-    full_abstracts = []
-    # Open each xml THIS IS VERY BRUTE FORCE AND WILL TAKE A WHILE
-    guessed_ids = []
-    db_files= os.listdir(pubmed_db_dir)
+def get_dataset_pmids(gold_dataset,loc_to_save_pmids):
+    dataset = None
+    with open(gold_dataset) as d:
+        dataset = json.loads(d.read())
+    questions = dataset["questions"]
+    pmids = set()
+    for question in questions:
+        documents = question.get("documents")
+        if documents:
+            ids = [url.rsplit('/', 1)[-1] for url in documents]
+            if ids:
+                pmids.update(ids)
+    # save the discovered pmids to file
+    print(f"saving {len(pmids)} PMIDs to {loc_to_save_pmids}")
+    with open(loc_to_save_pmids) as f:
+        pickle.dump(pmids,f)
+
+def map_abstracts_to_ids(pmids,database_dir,pmid_abstract_path):
+    print("Getting the abstract for each pmid")
+    pmids_set = pickle.load(pmids)
+    pmid_abstract_dict = dict()
+
+    #iterate through each db file
+    db_files= os.listdir(database_dir)
     for db_file in db_files:
-        full_path = pubmed_db_dir+ "/" + db_file
+        full_path = database_dir+ "/" + db_file
         print(f"opening db: {full_path}")
         pm_tree = ET.parse(full_path)
         if not pm_tree:
             print(f"ERROR: failed to open pubmed file {str(db_file)}")
             return
         root = pm_tree.getroot()
-        pubmed_articles = root.findall("PubmedArticle") 
+        pubmed_articles = root.findall("PubmedArticle")
         for article in pubmed_articles:
             try:
                 id = article.find("MedlineCitation").find("PMID").text
-                if id in ids:
+                if id in pmids_set:
                     try:
-                        full_abstracts.append(article.find('MedlineCitation').find('Article').find('Abstract').find('AbstractText').text)
+                        pmid_abstract_dict[id] = article.find('MedlineCitation').find('Article').find('Abstract').find('AbstractText').text
                     except Exception as e:
                         # no abstract for this article
                         print(f"no abstract for PMID {id}")
                         print(str(e))
-                    guessed_ids.append(id)
-                    ids.remove(id)
-                if not ids: # when we have found all the abstracts we are looking for
-                    return full_abstracts
             except Exception as e:
                 print(str(e)) 
-    return full_abstracts
+    print(f"saving {len(pmid_abstract_dict)} abstracts to {pmid_abstract_path}")
+    with open(pmid_abstract_path) as f:
+        pickle.dump(pmid_abstract_dict,f)
 
-
-# Retrieve the full PubMed abstracts for all articles identified in the golden dataset then augment it
-def get_full_abstracts(gold_dataset,augmented_dataset,pubmed_db_dir):
+def add_full_abstracts(old_dataset, pmid_map, new_dataset):
+    print(f"adding full_abstracts to {old_dataset} from {pmid_map} and saving as {new_dataset}")
+    pmid_abstract_dict = pickle.load(pmid_map)
     dataset = None
-    with open(gold_dataset) as d:
+    with open(old_dataset) as d:
         dataset = json.loads(d.read())
     questions = dataset["questions"]
-    n = 1
     for question in questions:
         documents = question.get("documents")
         print(f"question ({n} / {len(questions)})")
         if documents:
-            print(f"num documents: {len(documents)}")
-            extracted_abstracts = get_abstracts_from_db(documents,pubmed_db_dir)
-            question["full_abstracts"] = extracted_abstracts
-            print(f"found {len(extracted_abstracts)} full_abstracts")
+            ids = [url.rsplit('/', 1)[-1] for url in documents]
+            full_abs = [pmid_abstract_dict[id] for id in ids]
+            question["full_abstracts"] = full_abs
+            print(f"found ({len(full_abs)} / {len(ids)}) full_abstracts")
         n=n+1
-    with open(augmented_dataset,'w') as outfile:
+    with open(new_dataset,'w') as outfile:
         json.dump(dataset,outfile,indent=4)
+    print("Done!")
 
 if __name__ == "__main__":
     gold_dataset = "testing_datasets/BioASQ-training8b/training8b.json"
     augmented_dataset = "testing_datasets/BioASQ-training8b/augmented_test_FULL_ABSTRACTS.json"
-
     pubmed_db_dir = "umls/pubmed"
-    get_full_abstracts(gold_dataset, augmented_dataset, pubmed_db_dir)
+    pmids_path = "testing_datasets/pmids.txt"
+    pmid_abstract_path = "testing_datasets/pmids_and_abstracts.txt"
+
+    get_dataset_pmids(pubmed_db_dir,pmids_path)
+    map_abstracts_to_ids(pmids_path,pubmed_db_dir,pmid_abstract_path)
+    add_full_abstracts(gold_dataset, pmid_abstract_path, augmented_dataset)
